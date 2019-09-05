e8190e818341da3fc4297ef8cae5d63e1cc6a0f2
Merijn <merijn@hacktisch.com>
2019-09-05 10:53:03 +0200

    product schema

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+	product:require("./productSchema.js")
+}
---
+++ productSchema.js
@@ -0,0 +1,184 @@
+
+module.exports = [
+    {
+        tab: "main",
+        source: "title",
+        type: "text",
+        props: {
+            /////////validate: required
+        }
+    },
+
+    {
+        tab: "main",
+        source: "path.id",
+        type: "reference",
+        label: "Path",
+        reference: {
+            resource: "path",
+            optionText: "path",
+            inputType: "autocomplete"
+        },
+        props: {
+          ///////////  validate: required
+        }
+    },
+       {
+        tab: "main",
+        source: "published",
+        type: "boolean",
+        props:{
+            defaultValue:true
+        }
+    },
+    {
+        tab: "details",
+        source: "lead",
+        type: "longtext"
+    },
+    {
+        tab: "details",
+        source: "blurb",
+        type: "text"
+    },
+    {
+        tab: "details",
+        source: "body",
+        type: "richtext",
+        label: "Long description"
+    },
+    {
+        tab: "media",
+        label: "Main image",
+        source: "mainImage",
+        type: "image",
+        props: {
+         //////////   validate: required
+        }
+    },
+    {
+        tab: "media",
+        label: "Image alt attribute (SEO)",
+        source: "mainImage.alt",
+        type: "text"
+    },
+    {
+        tab: "media",
+        source: "slides",
+        type: "mediaplayer"
+        //slide imgs alt?...
+    },
+
+    {
+        tab: "catalog",
+        source: "price",
+        type: "number"
+    },
+    {
+        tab: "catalog",
+        source: "priceOnRequest",
+        type: "boolean"
+    },
+    {
+        tab: "details",
+        source: "reviews",
+        type: "array",
+        sub: [
+            {
+                source: "author",
+                type: "text"
+            },
+            {
+                source: "rating",
+                type: "number"
+            },
+            {
+                source: "text",
+                type: "longtext"
+            }
+        ]
+    },
+    {
+        tab: "details",
+        source: "titleOverwritten",
+        type: "text"
+    },
+    {
+        tab: "details",
+        source: "songs",
+        type: "array",
+        sub: [
+            {
+                source: "title",
+                type: "text"
+            },
+            {
+                source: "artist",
+                type: "text"
+            }
+        ]
+    },
+    {
+        tab: "catalog",
+        source: "weight",
+        type: "number"
+    },
+    {
+        tab: "catalog",
+        source: "isPartner",
+        type: "boolean"
+    },
+    {
+        tab: "catalog",
+        source: "category",
+        type: "reference",
+        props: {
+          /////////  validate: required
+        },
+        label: "Category",
+        root: "main",
+        reference: {
+            resource: "tree",
+            optionText: "text"
+        }
+    },
+    {
+        tab: "catalog",
+        source: "occasion",
+        type: "reference",
+        label: "Occasion",
+        root: "occasion",
+        reference: {
+            resource: "tree",
+            optionText: "text"
+        }
+    },
+    {
+        tab: "catalog",
+        source: "genre",
+        type: "reference",
+        label: "Genre",
+        root: "genre",
+        reference: {
+            resource: "tree",
+            optionText: "text"
+        }
+    },
+
+    /*
+ /*{
+        source:"related",
+        refs
+    },*/ {
+        tab: "meta",
+        source: "meta.title",
+        label: "Title meta tag",
+        type: "longtext"
+    },
+    {
+        tab: "meta",
+        source: "meta.description",
+        label: "Description meta tag",
+        type: "longtext"
+    }
+]
