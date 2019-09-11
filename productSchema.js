c98f25c0e0adb0930ee497c1355a32de7601a96d
Merijn <merijn@hacktisch.com>
2019-09-11 10:38:00 +0200

    productmeta

--- productSchema.js
+++ productSchema.js
@@ -162,16 +162,28 @@ module.exports = [
  /*{
         source:"related",
         refs
-    },*/ {
+    },*/ 
+
+
+{
         tab: "meta",
-        source: "meta.title",
-        label: "Title meta tag",
+        source: "meta",
+        label: "Meta tags",
+        type: "array",
+        min:1,
+        max:1,
+        sub: [
+            {
+                source: "title",
+                 label: "Title meta tag",
+                type: "longtext"
+            },
+            {
+                source: "description",
+               label: "Description meta tag",
         type: "longtext"
+            }
+        ]
     },
-    {
-        tab: "meta",
-        source: "meta.description",
-        label: "Description meta tag",
-        type: "longtext"
-    }
+    
 ];
