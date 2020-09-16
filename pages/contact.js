908ded85ef279c650bd373df3141af535efc8382
Merijn <merijn@hacktisch.com>
2020-09-16 15:57:02 +0200

    settings contact, front

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,68 @@
 module.exports = [
 
+
+	{ type: "text", source: "title", label: "Title" },
+	{
+		source: "header_img",
+		label: "Top image",
+
+		type: "image",
+		accept: {
+			"image/jpeg": true,
+			"image/webp": "jpg",
+			"image/png": "jpg"
+		},
+		transforms: {
+			_original: {
+				width: 1500,
+				height: 500
+			}
+		}
+	},
+	{ type: "richtext", source: "contact_info", label: "Contact info" },
+	{ type: "richtext", source: "address", label: "Address info" },
+	{
+		type: "array",
+
+		source: "team",
+		label: "Team",
+		sub: [
+			{
+				source: "portrait",
+				label: "Portrait photo",
+
+				type: "image",
+				accept: {
+					"image/jpeg": true,
+					"image/webp": "jpg",
+					"image/png": "jpg"
+				},
+				transforms: {
+					_original: {
+						width: 500,
+						height: 500
+					}
+				}
+			},
+			{
+				source: "name",
+				label: "Name",
+				type: "text"
+			},
+			{
+				source: "role",
+				label: "Job description",
+				type: "text"
+			},
+			{
+				source: "bio",
+				label: "Bio",
+				type: "richtext"
+			}
+		]
+	},
+
+
+
+    require("../groups/metaTags.js"),
 ];
--- pages/front.js
+++ pages/front.js
@@ -1,3 +1,133 @@
 module.exports = [
+	{ tab: "banner", source: "title", label: "Title", type: "text" },
+	{ tab: "banner", source: "desc", label: "Description", type: "longtext" },
+	{
+		tab: "banner",
+		source: "backgrounds",
+		label: "Backgrounds",
+		type: "array",
+		sub: [
+			{
+				source: "image",
+				label: "Image",
+				type: "image",
+				accept: {
+					"image/jpeg": true,
+					"image/webp": "jpg",
+					"image/png": "jpg"
+				},
+				transforms: {
+					_original: {
+						width: 1920,
+						height: 1080
+					}
+				}
+			}
+		]
+	},
 
+	{
+		tab: "banner",
+		source: "usp_quality",
+		label: "Usp: quality",
+		type: "text"
+	},
+	{
+		tab: "banner",
+		source: "usp_service",
+		label: "Usp: service",
+		type: "text"
+	},
+	{
+		tab: "banner",
+		source: "usp_guarantee",
+		label: "Usp: guarantee",
+		type: "text"
+	},
+	{
+		tab: "banner",
+		source: "usp_experience",
+		label: "Usp: experience",
+		type: "text"
+	},
+
+	{
+		tab: "order_process",
+		source: "process_title",
+		type: "text"
+	},
+	{
+		tab: "order_process",
+
+		source: "process",
+		label: "Steps",
+		type: "array",
+		min: 3,
+		max: 3,
+		sub: [
+			{
+				source: "image",
+				label: "Image",
+				type: "image",
+				accept: { "image/png": true, "image/webp": "png" },
+				transforms: {
+					_original: {
+						width: 300,
+						height: 300
+					}
+				}
+			}
+		]
+	},
+	{
+		tab: "product_selection",
+
+		source: "selection_title",
+		label: "Title above products",
+		type: "text"
+	},
+	{
+		tab: "logo_bar",
+
+		source: "logos",
+		label: "Logos",
+		type: "array",
+		sub: [
+			{
+				source: "image",
+				label: "Logo",
+				type: "image",
+				accept: { "image/png": true, "image/webp": "png" },
+				transforms: {
+					_original: {
+						width: 400,
+						height: 100
+					}
+				}
+			}
+		]
+	},
+	{
+		tab: "text",
+
+		source: "body",
+		label: "Body text",
+		type: "richtext"
+	},
+	{
+		tab: "text",
+
+		source: "button_contact",
+		label: "Text in contact button",
+		type: "text"
+	},
+	{
+		tab: "text",
+
+		source: "button_order",
+		label: "Text in order button",
+		type: "text"
+	},
+
+	require("../groups/metaTags.js")
 ];
