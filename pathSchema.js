9c65546859093e8280d99de49877264795f936da
Merijn <merijn@hacktisch.com>
2020-09-22 14:35:57 +0200

    regex validation

--- pathSchema.js
+++ pathSchema.js
@@ -3,6 +3,14 @@ module.exports = [
 		source: "path",
 		type: "text",
 		label: "Path",
-		validate: ["required"]
+		validate: [
+			"required",
+			{
+				type: "regex",
+				pattern: "^[a-zA-Z0-9-]([/]?[a-zA-Z0-9-]+)*$",
+				message:
+					"Only alphanumerical characters and hyphens allowed. Do not start or end with a slash. Double slashes (//) not allowed."
+			}
+		]
 	}
 ];
