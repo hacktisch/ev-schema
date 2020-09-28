fc40eb9b0d93939ded494c2368393a97fa6cf529
Merijn <merijn@hacktisch.com>
2020-09-28 16:54:17 +0200

    user table

---
+++ userSchema.js
@@ -0,0 +1,17 @@
+module.exports = [
+   {
+      
+        source: "username",
+        type: "text",
+        label: "Username"
+    },
+    {
+      
+        source: "password",
+        type: "password",
+        label: "Password"
+    },
+
+
+    require("./groups/changed.js")
+];
