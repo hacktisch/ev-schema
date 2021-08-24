8f7ee73cc165dff58745ed6121c86d4f6414ab43
Merijn <merijn@hacktisch.com>
2021-08-24 12:56:51 +0200

    warnWhen

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git push origin master && \
 
 version=$(git log --pretty=format:'%h' -n 1)
 echo "Installing version: $version to ev-api"
-cd ../ev-api && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/ev-schema.git#$version"
+cd ../ev-api && npm i "https://hacktisch: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@github.com/hacktisch/ev-schema.git#$version"
 echo "Installing version: $version to ev-cms"
-cd ../ev-cms && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/ev-schema.git#$version"
+cd ../ev-cms && npm i "https://hacktisch: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@github.com/hacktisch/ev-schema.git#$version"
